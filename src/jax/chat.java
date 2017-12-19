e50aa148c618ef126b51c2d1b5f1f0df0da1e767
ezzeddine <ezzeddine@gmail.com>
2017-12-19 19:22:50 +0100

    non avons ajouter noutre projet au repositery dans le git

---
+++ src/jax/chat.java
@@ -0,0 +1,33 @@
+package jax;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+@Path("/mem")
+@Produces("application/json")
+
+public class chat {
+	    private static Map<Integer, String> memoire = new HashMap<>();
+		@GET
+		public Map<Integer, String> getValue() {
+		return memoire;
+		}
+		@PUT
+		@Consumes("application/json")
+		public static void  insert(@QueryParam("id") Integer id, @QueryParam("val")
+		String val) {
+			
+		memoire.put(id, val);
+		
+		}
+		
+		
+		}
+
+
